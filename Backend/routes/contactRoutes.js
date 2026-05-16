851b699f1deb69e5e4a767cb6479ea39cd5aeffa
Sathiyaseelan16 <sathiyaseelan0603@gmail.com>
2026-05-16 23:03:46 +0530

    fix contact controller import case issue

--- Backend/routes/contactRoutes.js
+++ Backend/routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {sendMessage} = require("../controllers/ContactController");
+const {sendMessage} = require('../controllers/contactController');
 
 router.post("/", sendMessage);
 
